dd21bf86763f8256a8bf97e8a21045cf8843ed65
codeblad <152137808+codeblad@users.noreply.github.com>
2024-01-26 12:15:35 -0500

    Update Slam.java

--- src/main/java/radon/jujutsu_kaisen/ability/misc/Slam.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/Slam.java
@@ -27,7 +27,7 @@ import java.util.*;
 
 public class Slam extends Ability implements Ability.ICharged {
     private static final double RANGE = 30.0D;
-    private static final double LAUNCH_POWER = 3.0D;
+    private static final double LAUNCH_POWER = 2.5D;
     private static final float MAX_EXPLOSION = 5.0F;
 
     public static Map<UUID, Float> TARGETS = new HashMap<>();
@@ -119,6 +119,11 @@ public class Slam extends Ability implements Ability.ICharged {
     @Override
     public boolean onRelease(LivingEntity owner) {
         if (!owner.onGround()) {
+            if (!owner.level().isClientSide) {
+                TARGETS.put(owner.getUUID(), ((float) Math.min(20, this.getCharge(owner)) / 20));
+            }
+    
+            ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
             Vec3 target = this.getTarget(owner);
             owner.setDeltaMovement(owner.getDeltaMovement().add(target.subtract(owner.position()).normalize().scale(5.0D)));
             owner.swing(InteractionHand.MAIN_HAND);
