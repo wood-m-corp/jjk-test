2b8975270398e724e0074507229ef03576ed88bf
codeblad <152137808+codeblad@users.noreply.github.com>
2024-01-28 01:53:48 -0500

    Update FireArrow.java

--- src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/FireArrow.java
+++ src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/FireArrow.java
@@ -28,6 +28,23 @@ public class FireArrow extends Ability {
     }
 
     @Override
+    public int getPointsCost() {
+        return 50;
+    }
+
+    @Override
+    public Vec2 getDisplayCoordinates() {
+        return new Vec2(1.0F, 3.0F);
+    }
+
+    @Override
+    public boolean isDisplayed(LivingEntity owner) {
+        ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
+        CursedTechnique technique = cap.getTechnique();
+        return technique == CursedTechnique.DISASTER_FLAMES && super.isDisplayed(owner);
+    }
+
+    @Override
     public float getCost(LivingEntity owner) {
         return 350.0F;
     }
