846b349f74b025118567ff4b7a4ffdeb54f33555
codeblad <152137808+codeblad@users.noreply.github.com>
2024-01-27 19:36:21 -0500

    Update Infinity.java

--- src/main/java/radon/jujutsu_kaisen/ability/limitless/Infinity.java
+++ src/main/java/radon/jujutsu_kaisen/ability/limitless/Infinity.java
@@ -82,10 +82,6 @@ public class Infinity extends Ability implements Ability.IToggled {
 
     }
 
-    @Override
-    public int getCooldown() {
-        return 5 * 20;
-    }
 
     public static class FrozenProjectileData extends SavedData {
         public static final String IDENTIFIER = "frozen_projectile_data";
