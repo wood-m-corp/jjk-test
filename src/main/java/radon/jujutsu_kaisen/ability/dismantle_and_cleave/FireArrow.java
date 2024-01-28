ec0b315be5a3e6b95e57ba9426182c65f1d44ed4
codeblad <152137808+codeblad@users.noreply.github.com>
2024-01-28 02:04:17 -0500

    Update FireArrow.java

--- src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/FireArrow.java
+++ src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/FireArrow.java
@@ -7,6 +7,9 @@ import org.jetbrains.annotations.Nullable;
 import radon.jujutsu_kaisen.ability.base.Ability;
 import radon.jujutsu_kaisen.entity.projectile.FireArrowProjectile;
 import radon.jujutsu_kaisen.util.HelperMethods;
+import radon.jujutsu_kaisen.entity.effect.MeteorEntity;
+import radon.jujutsu_kaisen.capability.data.sorcerer.CursedTechnique;
+import net.minecraft.world.phys.Vec2;
 
 public class FireArrow extends Ability {
     @Override
