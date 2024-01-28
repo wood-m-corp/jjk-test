2e64dcb2048f0e047a17116c8db2994a5a39d7d2
codeblad <152137808+codeblad@users.noreply.github.com>
2024-01-28 14:36:27 -0500

    Update SorcererData.java

--- src/main/java/radon/jujutsu_kaisen/capability/data/SorcererData.java
+++ src/main/java/radon/jujutsu_kaisen/capability/data/SorcererData.java
@@ -487,7 +487,7 @@ public class SorcererData implements ISorcererData {
             EntityUtil.applyModifier(this.owner, Attributes.MOVEMENT_SPEED, MOVEMENT_SPEED_UUID, "Movement speed", Math.min(this.owner.getAttributeBaseValue(Attributes.MOVEMENT_SPEED) * 2,  movement), AttributeModifier.Operation.ADDITION);
 
             if (this.owner.getHealth() != this.owner.getMaxHealth()) {
-                this.owner.heal(0.75F / 20);
+                this.owner.heal(1.0F / 20);
             }
         } else {
             double health = Math.ceil(((this.getRealPower() - 1.0F) * 20.0D) / 20) * 20;
