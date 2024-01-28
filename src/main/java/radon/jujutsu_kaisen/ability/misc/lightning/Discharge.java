97b9fd8e7391d42836754f4f8d8c60b657136826
codeblad <152137808+codeblad@users.noreply.github.com>
2024-01-28 01:51:45 -0500

    Update Discharge.java

--- src/main/java/radon/jujutsu_kaisen/ability/misc/lightning/Discharge.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/lightning/Discharge.java
@@ -23,7 +23,7 @@ import radon.jujutsu_kaisen.util.HelperMethods;
 import radon.jujutsu_kaisen.util.RotationUtil;
 
 public class Discharge extends Ability implements Ability.IChannelened, Ability.IDurationable {
-    private static final float DAMAGE = 10.0F;
+    private static final float DAMAGE = 12.0F;
     private static final float MAX_RADIUS = 20.0F;
 
     @Override
