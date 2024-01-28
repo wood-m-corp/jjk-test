9903361ebdd035b1b452473adc947d8ab3665b78
codeblad <152137808+codeblad@users.noreply.github.com>
2024-01-28 03:41:59 -0500

    heavenly dura buff

--- src/main/java/radon/jujutsu_kaisen/event/JJKEventHandler.java
+++ src/main/java/radon/jujutsu_kaisen/event/JJKEventHandler.java
@@ -322,7 +322,7 @@ public class JJKEventHandler {
             float armor = cap.getExperience() * 0.01F;
 
             if (cap.hasTrait(Trait.HEAVENLY_RESTRICTION)) {
-                armor *= 3.0F;
+                armor *= 4.0F;
             }
             float blocked = CombatRules.getDamageAfterAbsorb(event.getAmount(), armor, armor * 0.1F);
             event.setAmount(blocked);
