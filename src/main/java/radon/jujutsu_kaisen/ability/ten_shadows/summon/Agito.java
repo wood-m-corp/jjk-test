81aac0334f065acce55a8ef9f1375449864d9acd
codeblad <152137808+codeblad@users.noreply.github.com>
2024-01-27 20:08:44 -0500

    Update Agito.java

--- src/main/java/radon/jujutsu_kaisen/ability/ten_shadows/summon/Agito.java
+++ src/main/java/radon/jujutsu_kaisen/ability/ten_shadows/summon/Agito.java
@@ -45,7 +45,7 @@ public class Agito extends Summon<AgitoEntity> {
 
     @Override
     public float getCost(LivingEntity owner) {
-        return 1.0F;
+        return 0.9F;
     }
 
     @Override
