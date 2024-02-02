bc7f318a4a1bac66d16afac602382b1b7b72e255
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-01 19:07:47 -0500

    Update JJKEventHandler.java

--- src/main/java/radon/jujutsu_kaisen/event/JJKEventHandler.java
+++ src/main/java/radon/jujutsu_kaisen/event/JJKEventHandler.java
@@ -279,7 +279,7 @@ public class JJKEventHandler {
             float armor = cap.getExperience() * 0.01F;
 
             if (cap.hasTrait(Trait.HEAVENLY_RESTRICTION)) {
-                armor *= 4.25F;
+                armor *= 3.5F;
             }
             float blocked = CombatRules.getDamageAfterAbsorb(event.getAmount(), armor, armor * 0.1F);
             event.setAmount(blocked);
