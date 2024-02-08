a9279fb1f6c1adc8582c9b03a696ce47df7a840d
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-07 19:25:40 -0500

    Update Heal.java

--- src/main/java/radon/jujutsu_kaisen/ability/misc/Heal.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/Heal.java
@@ -36,11 +36,12 @@ public class Heal extends Ability implements Ability.IChannelened {
     @Override
     public void run(LivingEntity owner) {
         if (owner instanceof Player player) {
-        owner.heal((float) Math.min(1.0F, ConfigHolder.SERVER.curseHealingAmount.get().floatValue() * Math.pow(this.getPower(owner) * 0.225F, Math.log(this.getPower(owner))) * 0.225F));
+            owner.heal((float) Math.min(1.0F, ConfigHolder.SERVER.curseHealingAmount.get().floatValue() * Math.pow(this.getPower(owner) * 0.225F, Math.log(this.getPower(owner))) * 0.225F));
         }
         else {
-        owner.heal((float) Math.min(1.0F, ConfigHolder.SERVER.curseHealingAmount.get().floatValue() * Math.pow(this.getPower(owner) * 0.075F, Math.log(this.getPower(owner))) * 0.075F));
+            owner.heal((float) Math.min(1.0F, ConfigHolder.SERVER.curseHealingAmount.get().floatValue() * Math.pow(this.getPower(owner) * 0.075F, Math.log(this.getPower(owner))) * 0.075F));
         }
+    }
 
     @Override
     public float getCost(LivingEntity owner) {
