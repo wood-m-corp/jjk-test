2b7dd02cc6f82f62a238871f444222bca1dffe69
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-09 17:42:07 -0500

    nerfed

--- src/main/java/radon/jujutsu_kaisen/entity/effect/AirFrameEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/effect/AirFrameEntity.java
@@ -32,7 +32,7 @@ import java.util.UUID;
 
 public class AirFrameEntity extends Entity {
     private static final int DURATION = 15;
-    private static final float DAMAGE = 22.0F;
+    private static final float DAMAGE = 18.0F;
     private static final double RANGE = 5.0D;
 
     @Nullable
