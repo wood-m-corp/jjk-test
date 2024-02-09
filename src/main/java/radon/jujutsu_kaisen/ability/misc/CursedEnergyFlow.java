3400fbdb69c3db334357a66a664a2b3bd283bd4e
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-09 14:48:33 -0500

    CE punch buff

--- src/main/java/radon/jujutsu_kaisen/ability/misc/CursedEnergyFlow.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/CursedEnergyFlow.java
@@ -201,7 +201,7 @@ public class CursedEnergyFlow extends Ability implements Ability.IToggled {
                     ISorcererData attackerCap = attacker.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
 
                     if (HelperMethods.isMelee(source)) {
-                        float increase = attackerCap.getExperience() * 0.0009F;
+                        float increase = attackerCap.getExperience() * 0.002F;
 
                         switch (attackerCap.getNature()) {
                             case ROUGH -> increase *= 1.5F;
