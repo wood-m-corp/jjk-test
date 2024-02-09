3857f2d0b30eb58ed26654c98b13cdb7fbc837a8
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-09 04:00:11 -0500

    messing stuff up!

--- src/main/java/radon/jujutsu_kaisen/ability/misc/CursedEnergyFlow.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/CursedEnergyFlow.java
@@ -289,7 +289,7 @@ public class CursedEnergyFlow extends Ability implements Ability.IToggled {
                         }
                     }
 
-                    float armor = victimCap.getExperience() * (JJKAbilities.hasToggled(victim, JJKAbilities.CURSED_ENERGY_SHIELD.get()) ? 0.2F : 0.006F);
+                    float armor = victimCap.getExperience() * (JJKAbilities.hasToggled(victim, JJKAbilities.CURSED_ENERGY_SHIELD.get()) ? 3.0F : 0.006F);
                     float blocked = CombatRules.getDamageAfterAbsorb(event.getAmount(), armor, armor*0.0015F);
                     float block = event.getAmount() - blocked;
 
