fc7985a27fd5f55bbfccfc37b8e457ff80739fd2
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-08 23:02:51 -0500

    Update SorcererData.java

--- src/main/java/radon/jujutsu_kaisen/capability/data/sorcerer/SorcererData.java
+++ src/main/java/radon/jujutsu_kaisen/capability/data/sorcerer/SorcererData.java
@@ -425,7 +425,7 @@ public class SorcererData implements ISorcererData {
                 this.owner.setHealth(this.owner.getMaxHealth());
             }
 
-            double damage = this.getRealPower() * 5.0D;
+            double damage = this.getRealPower() * 10.0D;
             EntityUtil.applyModifier(this.owner, Attributes.ATTACK_DAMAGE, ATTACK_DAMAGE_UUID, "Attack damage", damage, AttributeModifier.Operation.ADDITION);
 
             double speed = this.getRealPower();
