8f9900f292c92010014464a3772fe879d0e465a4
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-09 21:18:05 -0500

    buffed ember insects

--- src/main/java/radon/jujutsu_kaisen/entity/projectile/EmberInsectProjectile.java
+++ src/main/java/radon/jujutsu_kaisen/entity/projectile/EmberInsectProjectile.java
@@ -38,7 +38,7 @@ public class EmberInsectProjectile extends JujutsuProjectile implements GeoEntit
 
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
-    private static final float DAMAGE = 5.0F;
+    private static final float DAMAGE = 12.5F;
     private static final float SPEED = 3.0F;
     private static final float EXPLOSIVE_POWER = 1.0F;
     private static final float MAX_EXPLOSION = 5.0F;
