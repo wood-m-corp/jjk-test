190ef1799d7005afbb4c81b3c94a08d1731b4f21
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-09 21:55:29 -0500

    deer buff

--- src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/TranquilDeerEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/TranquilDeerEntity.java
@@ -105,7 +105,7 @@ public class TranquilDeerEntity extends TenShadowsSummon {
     public static AttributeSupplier.Builder createAttributes() {
         return SorcererEntity.createAttributes()
                 .add(Attributes.MAX_HEALTH, 3 * 20.0D)
-                .add(Attributes.ATTACK_DAMAGE, 3 * 2.0D);
+                .add(Attributes.ATTACK_DAMAGE, 5 * 4.0D);
     }
 
     private PlayState walkRunIdlePredicate(AnimationState<TranquilDeerEntity> animationState) {
