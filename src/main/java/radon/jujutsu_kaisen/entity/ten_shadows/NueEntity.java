abad2bc6e4468665a35967bf6e064b9540d54ec4
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-09 23:09:29 -0500

    nerf

--- src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/NueEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/NueEntity.java
@@ -105,7 +105,7 @@ public class NueEntity extends TenShadowsSummon implements PlayerRideable, IJump
         return SorcererEntity.createAttributes()
                 .add(Attributes.FLYING_SPEED)
                 .add(Attributes.MAX_HEALTH, 2 * 25.0D)
-                .add(Attributes.ATTACK_DAMAGE, 12 * 5.5D);
+                .add(Attributes.ATTACK_DAMAGE, 12 * 4.75D);
     }
 
     @Override
