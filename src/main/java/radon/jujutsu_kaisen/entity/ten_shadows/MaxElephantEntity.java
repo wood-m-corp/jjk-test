813213654bd5ef7fae64de10a44daefb290ccdfd
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-09 21:54:08 -0500

    max elephant dmg buff

--- src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/MaxElephantEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/MaxElephantEntity.java
@@ -186,7 +186,7 @@ public class MaxElephantEntity extends TenShadowsSummon implements PlayerRideabl
         return SorcererEntity.createAttributes()
                 .add(Attributes.MOVEMENT_SPEED, 0.33D)
                 .add(Attributes.MAX_HEALTH, 3 * 20.0D)
-                .add(Attributes.ATTACK_DAMAGE, 3 * 2.0D);
+                .add(Attributes.ATTACK_DAMAGE, 6 * 3.5D);
     }
 
     private PlayState walkRunIdlePredicate(AnimationState<MaxElephantEntity> animationState) {
