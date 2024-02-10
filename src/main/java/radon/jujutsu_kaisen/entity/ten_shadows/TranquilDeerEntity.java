5a77e6e886ec4efc10ddf493e2cf1753d1f7d8c0
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-10 15:28:23 -0500

    nerf

--- src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/TranquilDeerEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/TranquilDeerEntity.java
@@ -105,7 +105,7 @@ public class TranquilDeerEntity extends TenShadowsSummon {
     public static AttributeSupplier.Builder createAttributes() {
         return SorcererEntity.createAttributes()
                 .add(Attributes.MAX_HEALTH, 4 * 20.0D)
-                .add(Attributes.ATTACK_DAMAGE, 12 * 3.0D);
+                .add(Attributes.ATTACK_DAMAGE, 6 * 2.5D);
     }
 
     private PlayState walkRunIdlePredicate(AnimationState<TranquilDeerEntity> animationState) {
