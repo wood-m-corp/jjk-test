7bb4287968db5601981df1f440f3397ee778f90d
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-10 16:19:45 -0500

    Update CursedEnergyFlow.java

--- src/main/java/radon/jujutsu_kaisen/ability/misc/CursedEnergyFlow.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/CursedEnergyFlow.java
@@ -281,7 +281,7 @@ public class CursedEnergyFlow extends Ability implements Ability.IToggled {
                         switch (victimCap.getNature()) {
                             case LIGHTNING -> attacker.addEffect(new MobEffectInstance(JJKEffects.STUN.get(), 20, 0,
                                     false, false, false));
-                            case ROUGH -> attacker.hurt(JJKDamageSources.jujutsuAttack(victim, this) 0.5F + (victimCap.getExperience() * 0.001)
+                            case ROUGH -> attacker.hurt(JJKDamageSources.jujutsuAttack(victim, null) 0.5F + (victimCap.getExperience() * 0.001)
                                 }
                             }
                         }
