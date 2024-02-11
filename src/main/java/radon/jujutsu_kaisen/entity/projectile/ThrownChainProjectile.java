c3d65ca2c69e2679c9e53d28a0d64c613d1f4835
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-10 23:45:13 -0500

    Update ThrownChainProjectile.java

--- src/main/java/radon/jujutsu_kaisen/entity/projectile/ThrownChainProjectile.java
+++ src/main/java/radon/jujutsu_kaisen/entity/projectile/ThrownChainProjectile.java
@@ -136,7 +136,7 @@ public class ThrownChainProjectile extends AbstractArrow {
 
             float speed = this.getDeltaMovement().lengthSqr();
             ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
-            DAMAGE = 9.0F*cap.getPower(owner);
+            float DAMAGE = 9.0F*cap.getPower(owner);
 
             SwordItem sword = (SwordItem) this.getStack().getItem();
             target.hurt(source, (float) (sword.getDamage() + DAMAGE));
@@ -185,7 +185,7 @@ public class ThrownChainProjectile extends AbstractArrow {
 
                 if (this.dealtDamage && this.pulled != null) {
                     if (this.pulled == living) {
-                        speedMult/=2.5F;
+                        speedMult*=0.4F;
                         living.setDeltaMovement(this.position().subtract(living.position()).normalize()*speedMult);
 
                         if (living.distanceTo(this) <= 1.0D) {
@@ -194,7 +194,7 @@ public class ThrownChainProjectile extends AbstractArrow {
                         }
                     } else {
                         this.setPos(this.pulled.position().add(0.0D, this.pulled.getBbHeight() / 2.0F, 0.0D));
-                        speedMult/=2.5F;
+                        speedMult*=0.4F;
                         this.pulled.setDeltaMovement(living.position().subtract(this.pulled.position()).normalize()*speedMult);
 
                         if (this.pulled.distanceTo(living) <= 1.0D) {
