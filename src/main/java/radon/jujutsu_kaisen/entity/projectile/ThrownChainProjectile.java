6f9baeb0a4d0c2d10e459ca4b898c8606a021c50
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-10 23:54:04 -0500

    Update ThrownChainProjectile.java

--- src/main/java/radon/jujutsu_kaisen/entity/projectile/ThrownChainProjectile.java
+++ src/main/java/radon/jujutsu_kaisen/entity/projectile/ThrownChainProjectile.java
@@ -186,7 +186,7 @@ public class ThrownChainProjectile extends AbstractArrow {
                 if (this.dealtDamage && this.pulled != null) {
                     if (this.pulled == living) {
                         speedMult*=0.4F;
-                        living.setDeltaMovement((this.position().subtract(living.position())*speedMult).normalize());
+                        living.setDeltaMovement(this.position().subtract(living.position()).normalize()*speedMult);
 
                         if (living.distanceTo(this) <= 1.0D) {
                             this.discard();
@@ -195,7 +195,7 @@ public class ThrownChainProjectile extends AbstractArrow {
                     } else {
                         this.setPos(this.pulled.position().add(0.0D, this.pulled.getBbHeight() / 2.0F, 0.0D));
                         speedMult*=0.4F;
-                        this.pulled.setDeltaMovement((living.position().subtract(this.pulled.position())*speedMult).normalize());
+                        this.pulled.setDeltaMovement(living.position().subtract(this.pulled.position()).normalize()*speedMult);
 
                         if (this.pulled.distanceTo(living) <= 1.0D) {
                             this.discard();
