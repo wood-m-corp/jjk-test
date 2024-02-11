f7eb3d955ef73df8343e309b7e83f6d4241f02bf
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-10 22:55:35 -0500

    Update PolymorphicSoulIsomerEntity.java

--- src/main/java/radon/jujutsu_kaisen/entity/idle_transfiguration/PolymorphicSoulIsomerEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/idle_transfiguration/PolymorphicSoulIsomerEntity.java
@@ -24,7 +24,7 @@ public class PolymorphicSoulIsomerEntity extends TransfiguredSoulEntity {
     public static AttributeSupplier.Builder createAttributes() {
         return SorcererEntity.createAttributes()
                 .add(Attributes.MAX_HEALTH, 5 * 25.0F)
-                .add(Attributes.ATTACK_DAMAGE, 5 * 2.0D);
+                .add(Attributes.ATTACK_DAMAGE, 6 * 4.0D);
     }
 
     @Override
