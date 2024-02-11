a71bc6362656030a2a8548f49b5337d2848b32fb
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-10 19:20:05 -0500

    Update LightningEntity.java

--- src/main/java/radon/jujutsu_kaisen/entity/effect/LightningEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/effect/LightningEntity.java
@@ -29,7 +29,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class LightningEntity extends JujutsuProjectile {
-    public static final float SCALE = 2.0F;
+    public static final float SCALE = 5.0F;
     public static final double RANGE = 20;
 
     public double endPosX, endPosY, endPosZ;
@@ -107,7 +107,7 @@ public class LightningEntity extends JujutsuProjectile {
                                 this.getDamage() * this.getPower());
                     }
 
-                    double radius = SCALE * 2.0F;
+                    double radius = SCALE * 0.75F;
 
                     AABB bounds = new AABB(this.collidePosX - radius, this.collidePosY - radius, this.collidePosZ - radius,
                             this.collidePosX + radius, this.collidePosY + radius, this.collidePosZ + radius);
