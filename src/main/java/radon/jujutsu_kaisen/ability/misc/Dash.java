c72785aa233f5a56c9deaac4298b1d5e04078697
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-11 04:14:20 -0500

    Update Dash.java

--- src/main/java/radon/jujutsu_kaisen/ability/misc/Dash.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/Dash.java
@@ -186,7 +186,7 @@ public class Dash extends Ability {
         ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
 
         if (cap.hasTrait(Trait.HEAVENLY_RESTRICTION)) {
-            return 0.15D;
+            return 2;
         }
         return super.getRealCooldown(owner);
     }
