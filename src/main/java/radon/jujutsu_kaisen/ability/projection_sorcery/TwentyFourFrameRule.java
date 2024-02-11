8949713b46992f688e99a88db3e16c231cc1aa70
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-11 18:36:11 -0500

    Update TwentyFourFrameRule.java

--- src/main/java/radon/jujutsu_kaisen/ability/projection_sorcery/TwentyFourFrameRule.java
+++ src/main/java/radon/jujutsu_kaisen/ability/projection_sorcery/TwentyFourFrameRule.java
@@ -53,7 +53,7 @@ public class TwentyFourFrameRule extends Ability implements Ability.IToggled, Ab
 
     @Override
     public float getCost(LivingEntity owner) {
-        return 50.0F;
+        return 60.0F;
     }
 
     @Override
