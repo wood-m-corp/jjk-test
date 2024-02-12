665ca73fcaa497a6aa32dda8254894dd3f742862
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-11 20:35:33 -0500

    Update Teleport.java

--- src/main/java/radon/jujutsu_kaisen/ability/limitless/Teleport.java
+++ src/main/java/radon/jujutsu_kaisen/ability/limitless/Teleport.java
@@ -17,7 +17,7 @@ import radon.jujutsu_kaisen.util.HelperMethods;
 import radon.jujutsu_kaisen.util.RotationUtil;
 
 public class Teleport extends Ability {
-    private static final double RANGE = 80.0D;
+    private static final double RANGE = 70.0D;
 
     @Override
     public boolean isScalable(LivingEntity owner) {
