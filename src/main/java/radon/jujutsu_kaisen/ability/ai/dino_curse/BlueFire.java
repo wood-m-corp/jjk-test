6438eb3a0a982bf02791a01b2c6fc1a415613ca9
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-11 23:02:21 -0500

    Update BlueFire.java

--- src/main/java/radon/jujutsu_kaisen/ability/ai/dino_curse/BlueFire.java
+++ src/main/java/radon/jujutsu_kaisen/ability/ai/dino_curse/BlueFire.java
@@ -33,7 +33,7 @@ import java.util.Optional;
 public class BlueFire extends Ability implements Ability.IChannelened, Ability.IDurationable {
     public static final double RANGE = 32;
     private static final float SCALE = 5.0F;
-    private static final float DAMAGE = 10.0F;
+    private static final float DAMAGE = 9.0F;
 
     @Override
     public boolean isScalable(LivingEntity owner) {
