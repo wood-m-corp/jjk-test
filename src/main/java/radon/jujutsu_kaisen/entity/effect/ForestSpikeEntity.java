58d35850dcee837aea941dbce76465a60b16a4f2
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-11 22:10:01 -0500

    Update ForestSpikeEntity.java

--- src/main/java/radon/jujutsu_kaisen/entity/effect/ForestSpikeEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/effect/ForestSpikeEntity.java
@@ -15,7 +15,7 @@ import radon.jujutsu_kaisen.util.RotationUtil;
 
 public class ForestSpikeEntity extends JujutsuProjectile {
     private static final int DURATION = 5 * 20;
-    private static final float DAMAGE = 17.5F;
+    private static final float DAMAGE = 16.0F;
 
     public ForestSpikeEntity(EntityType<? extends Projectile> pType, Level pLevel) {
         super(pType, pLevel);
