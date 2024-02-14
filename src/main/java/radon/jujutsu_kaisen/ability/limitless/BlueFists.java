f8be02e912708b2d3e7748bda5950c4769d81cd5
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-14 15:55:43 -0500

    Update BlueFists.java

--- src/main/java/radon/jujutsu_kaisen/ability/limitless/BlueFists.java
+++ src/main/java/radon/jujutsu_kaisen/ability/limitless/BlueFists.java
@@ -45,7 +45,7 @@ public class BlueFists extends Ability implements Ability.IToggled, Ability.IAtt
 
     @Override
     public float getCost(LivingEntity owner) {
-        return 25.0F;
+        return 50.0F;
     }
 
     @Override
