0fd1db7b3635ad7465181e2e84e28d88449917f2
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-14 13:45:27 -0500

    Update CursedEnergyFlow.java

--- src/main/java/radon/jujutsu_kaisen/ability/misc/CursedEnergyFlow.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/CursedEnergyFlow.java
@@ -202,7 +202,7 @@ public class CursedEnergyFlow extends Ability implements Ability.IToggled {
                     ISorcererData attackerCap = attacker.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
 
                     if (HelperMethods.isMelee(source)) {
-                        float increase = attackerCap.getExperience() * 0.002F;
+                        float increase = attackerCap.getExperience() * 0.00135F;
 
                         switch (attackerCap.getNature()) {
                             case ROUGH -> increase *= 1.6F;
