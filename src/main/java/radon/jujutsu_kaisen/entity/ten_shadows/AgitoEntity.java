965f3f2c6e96510b5d072861bc473247717d74c4
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-14 15:53:21 -0500

    Update AgitoEntity.java

--- src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/AgitoEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/AgitoEntity.java
@@ -110,9 +110,9 @@ public class AgitoEntity extends TenShadowsSummon {
 
     public static AttributeSupplier.Builder createAttributes() {
         return SorcererEntity.createAttributes()
-                .add(Attributes.MAX_HEALTH, 5 * 20.0D)
-                .add(Attributes.ARMOR, 20.0D)
-                .add(Attributes.ATTACK_DAMAGE, 7 * 2.75D);
+                .add(Attributes.MAX_HEALTH, 5 * 14.0D)
+                .add(Attributes.ARMOR, 16.0D)
+                .add(Attributes.ATTACK_DAMAGE, 7 * 2.0D);
         
     }
 
