0cce4a47f692bb248a834426310d7b79e570d7a1
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-14 16:00:02 -0500

    Update Spiderweb.java

--- src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/Spiderweb.java
+++ src/main/java/radon/jujutsu_kaisen/ability/dismantle_and_cleave/Spiderweb.java
@@ -19,9 +19,9 @@ import radon.jujutsu_kaisen.util.HelperMethods;
 import radon.jujutsu_kaisen.util.RotationUtil;
 
 public class Spiderweb extends Ability {
-    private static final int RANGE = 5;
+    private static final int RANGE = 6;
     private static final int DELAY = 20;
-    private static final float EXPLOSIVE_POWER = 2.0F;
+    private static final float EXPLOSIVE_POWER = 3.0F;
     private static final float MAX_EXPLOSIVE_POWER = 30.0F;
 
     @Override
