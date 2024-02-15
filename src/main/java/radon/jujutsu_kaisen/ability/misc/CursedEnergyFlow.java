e5e70a658d3194f1a62b4413a28f19712e2c3403
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-15 13:07:45 -0500

    Update CursedEnergyFlow.java

--- src/main/java/radon/jujutsu_kaisen/ability/misc/CursedEnergyFlow.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/CursedEnergyFlow.java
@@ -246,7 +246,7 @@ public class CursedEnergyFlow extends Ability implements Ability.IToggled {
 
                     if (JJKAbilities.hasToggled(attacker, JJKAbilities.CURSED_ENERGY_FLOW.get())) {
                         if (attackerCap.getNature() == CursedEnergyNature.LIGHTNING) {
-                            victim.addEffect(new MobEffectInstance(JJKEffects.STUN.get(), 10 * (attacker.getItemInHand(InteractionHand.MAIN_HAND).is(JJKItems.NYOI_STAFF.get()) ? 2 : 1), 0, false, false, false));
+                            victim.addEffect(new MobEffectInstance(JJKEffects.STUN.get(), 10 * (attacker.getItemInHand(InteractionHand.MAIN_HAND).is(JJKItems.NYOI_STAFF.get()) ? 1.5 : 1), 0, false, false, false));
                             victim.playSound(SoundEvents.LIGHTNING_BOLT_IMPACT, 1.0F, 0.5F + HelperMethods.RANDOM.nextFloat() * 0.2F);
 
                             if (!attacker.level().isClientSide) {
