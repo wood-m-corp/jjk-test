61e8d89f88402fef45fb19ea886989cc78b2021c
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-15 16:30:25 -0500

    Update NueTotality.java

--- src/main/java/radon/jujutsu_kaisen/ability/ten_shadows/summon/NueTotality.java
+++ src/main/java/radon/jujutsu_kaisen/ability/ten_shadows/summon/NueTotality.java
@@ -43,7 +43,7 @@ public class NueTotality extends Summon<NueTotalityEntity> {
 
     @Override
     public float getCost(LivingEntity owner) {
-        return 0.75F;
+        return 0.8F;
     }
 
     @Override
