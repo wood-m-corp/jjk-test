3221b6556e0f120a674c119097f591ac72d81113
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-15 18:42:33 -0500

    Update Punch.java

--- src/main/java/radon/jujutsu_kaisen/ability/misc/Punch.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/Punch.java
@@ -27,7 +27,7 @@ import java.util.List;
 
 public class Punch extends Ability {
     private static final float DAMAGE = 7.5F;
-    private static final double RANGE = 6.0D;
+    private static final double RANGE = 5.0D;
     private static final double LAUNCH_POWER = 2.5D;
 
     @Override
