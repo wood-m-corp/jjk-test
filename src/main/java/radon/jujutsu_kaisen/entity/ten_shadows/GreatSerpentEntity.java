7e22f227718a9f67ebc2a82d0bedecb0eefaecbc
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-15 11:59:55 -0500

    Update GreatSerpentEntity.java

--- src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/GreatSerpentEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/GreatSerpentEntity.java
@@ -251,7 +251,7 @@ public class GreatSerpentEntity extends TenShadowsSummon {
     public static AttributeSupplier.Builder createAttributes() {
         return SorcererEntity.createAttributes()
                 .add(Attributes.MAX_HEALTH, 3 * 20.0D)
-                .add(Attributes.MOVEMENT_SPEED, 1.25D)
+                .add(Attributes.MOVEMENT_SPEED, 0.5D)
                 .add(Attributes.ARMOR, 10.0D)
                 .add(Attributes.ATTACK_DAMAGE, 5 * 4.0D);
     }
