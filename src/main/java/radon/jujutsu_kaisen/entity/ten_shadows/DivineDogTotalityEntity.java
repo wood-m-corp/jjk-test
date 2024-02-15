2d715064783aa99e61fdcac0f16ca79ceb83816c
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-15 12:16:20 -0500

    Update DivineDogTotalityEntity.java

--- src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/DivineDogTotalityEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/DivineDogTotalityEntity.java
@@ -75,7 +75,7 @@ public class DivineDogTotalityEntity extends TenShadowsSummon {
     }
 
     public static AttributeSupplier.Builder createAttributes() {
-        return SorcererEntity.createAttributes().add(Attributes.MOVEMENT_SPEED, 0.5D)
+        return SorcererEntity.createAttributes().add(Attributes.MOVEMENT_SPEED, 1.5 * 0.33D)
                 .add(Attributes.MAX_HEALTH, 4 * 13.0D)
                 .add(Attributes.ARMOR, 14.0D)
                 .add(Attributes.ATTACK_DAMAGE, 5 * 4.5D);
