ac8cb3b3dad4873c4452dcd7b64a05875f63e6e6
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-15 16:34:16 -0500

    Update ToadEntity.java

--- src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/ToadEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/ToadEntity.java
@@ -44,7 +44,7 @@ public class ToadEntity extends TenShadowsSummon {
     private static final RawAnimation HOWL = RawAnimation.begin().thenPlayAndHold("misc.howl");
 
     private static final int RANGE = 40;
-    private static final int SHOOT_INTERVAL = 100;
+    private static final int SHOOT_INTERVAL = 150;
 
     public ToadEntity(EntityType<? extends TamableAnimal> pType, Level pLevel) {
         super(pType, pLevel);
