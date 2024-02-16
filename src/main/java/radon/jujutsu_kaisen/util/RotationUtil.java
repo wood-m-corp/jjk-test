343142e6d2e1313476dd75f0fa0b492b3aecab2e
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-16 15:08:14 -0500

    Update RotationUtil.java

--- src/main/java/radon/jujutsu_kaisen/util/RotationUtil.java
+++ src/main/java/radon/jujutsu_kaisen/util/RotationUtil.java
@@ -151,7 +151,7 @@ public class RotationUtil {
         if (blockHit.getType() != HitResult.Type.MISS) {
             end = blockHit.getLocation();
         }
-        AABB bounds = AABB.ofSize(start,4.0D,4.0D,1.0D).expandTowards(end.subtract(start)).inflate(2.0D);
+        AABB bounds = AABB.ofSize(start,2.0D,2.0D,2.0D).expandTowards(end.subtract(start)).inflate(2.0D);
         LivingEntity targeted = null;
         for (LivingEntity select : level.getEntitiesOfClass(LivingEntity.class, bounds,
             select -> select != entity )) {
