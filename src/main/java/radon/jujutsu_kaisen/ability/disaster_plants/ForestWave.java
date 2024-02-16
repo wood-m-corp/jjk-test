c915849b1b1322d5ef60628e86ee78bdf584236f
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-16 18:49:53 -0500

    Update ForestWave.java

--- src/main/java/radon/jujutsu_kaisen/ability/disaster_plants/ForestWave.java
+++ src/main/java/radon/jujutsu_kaisen/ability/disaster_plants/ForestWave.java
@@ -14,7 +14,7 @@ import radon.jujutsu_kaisen.util.RotationUtil;
 
 public class ForestWave extends Ability implements Ability.IChannelened, Ability.IDurationable {
     private static final int DELAY = 2;
-    private static final int SPEED = 20;
+    private static final int SPEED = 10;
 
     @Override
     public boolean shouldTrigger(PathfinderMob owner, @Nullable LivingEntity target) {
