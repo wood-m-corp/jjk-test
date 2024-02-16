38c276d368a64278852c78e29dd7a403d76c567b
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-16 18:31:45 -0500

    Update ForestWave.java

--- src/main/java/radon/jujutsu_kaisen/ability/disaster_plants/ForestWave.java
+++ src/main/java/radon/jujutsu_kaisen/ability/disaster_plants/ForestWave.java
@@ -77,7 +77,7 @@ public class ForestWave extends Ability implements Ability.IChannelened, Ability
 
     @Override
     public float getCost(LivingEntity owner) {
-        return 1.0F;
+        return 4.0F;
     }
 
     @Override
