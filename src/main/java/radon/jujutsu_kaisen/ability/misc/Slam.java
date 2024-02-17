a3b372f6e59969fffa2c0bcd41362dc745874d69
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-17 02:32:36 -0500

    Update Slam.java

--- src/main/java/radon/jujutsu_kaisen/ability/misc/Slam.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/Slam.java
@@ -160,7 +160,7 @@ public class Slam extends Ability implements Ability.ICharged {
         
                 cap.delayTickEvent(() -> {
                     Vec3 target = this.getTarget(owner);
-                    owner.setDeltaMovement(owner.getDeltaMovement().add(target.subtract(owner.position()).normalize().scale(launchPower)));
+                    owner.setDeltaMovement(owner.getDeltaMovement().add(target.subtract(owner.position()).normalize().scale(launchPower)).multiply(1.0D, 0.25D, 1.0D));
                     cap.delayTickEvent(() -> {
                         TARGETS.remove(owner.getUUID());
                     }, 20*3);
