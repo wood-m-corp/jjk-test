98e28fc2ff4578e3dcae4519b1abc26157129dfa
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-17 20:31:20 -0500

    Update ForestWave.java

--- src/main/java/radon/jujutsu_kaisen/ability/disaster_plants/ForestWave.java
+++ src/main/java/radon/jujutsu_kaisen/ability/disaster_plants/ForestWave.java
@@ -87,7 +87,7 @@ public class ForestWave extends Ability implements Ability.IChannelened, Ability
 
     @Override
     public int getDuration() {
-        return 4.0F;
+        return 4;
     }
 
     @Override
