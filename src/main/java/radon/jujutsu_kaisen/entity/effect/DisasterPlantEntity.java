a3f263a33b478773000d70ed945727150d6753f7
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-17 21:10:47 -0500

    Update DisasterPlantEntity.java

--- src/main/java/radon/jujutsu_kaisen/entity/effect/DisasterPlantEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/effect/DisasterPlantEntity.java
@@ -42,7 +42,7 @@ public class DisasterPlantEntity extends JujutsuProjectile implements GeoEntity 
     }
 
     public DisasterPlantEntity(LivingEntity owner, float power, LivingEntity target) {
-        super(JJKEntities.DISASTER_PLANT.get(), owner.level(), owner, power * 0.3F);
+        super(JJKEntities.DISASTER_PLANT.get(), owner.level(), owner, power * 0.4F);
 
         this.setTarget(target);
 
