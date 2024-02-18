79b63961b9ef91e8292b21c5841db506b30bb926
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-17 20:19:48 -0500

    Update GetCrushed.java

--- src/main/java/radon/jujutsu_kaisen/ability/cursed_speech/GetCrushed.java
+++ src/main/java/radon/jujutsu_kaisen/ability/cursed_speech/GetCrushed.java
@@ -9,6 +9,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.AABB;
