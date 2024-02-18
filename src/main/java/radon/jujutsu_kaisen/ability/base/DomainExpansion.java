ba7b82e4143ad7d261ac234d96dfeff6b9020587
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-17 19:26:01 -0500

    Update DomainExpansion.java

--- src/main/java/radon/jujutsu_kaisen/ability/base/DomainExpansion.java
+++ src/main/java/radon/jujutsu_kaisen/ability/base/DomainExpansion.java
@@ -91,7 +91,8 @@ public abstract class DomainExpansion extends Ability implements Ability.IToggle
 
     public static float getStrength(LivingEntity owner, boolean instant) {
         ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
-        return ((ConfigHolder.SERVER.maximumDomainSize.get().floatValue() + 0.1F) - cap.getDomainSize()) * (instant ? 0.5F : 1.0F);
+        float size = cap.getDomainSize() * 0.65F;
+        return ((ConfigHolder.SERVER.maximumDomainSize.get().floatValue() + 0.1F) - size * (instant ? 0.5F : 1.0F);
     }
 
     @Override
