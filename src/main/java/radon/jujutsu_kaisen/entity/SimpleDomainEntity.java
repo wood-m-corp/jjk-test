0e1e3af2aa3740cd08f66e45af0f91eea5179130
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-20 18:07:06 -0500

    Update SimpleDomainEntity.java

--- src/main/java/radon/jujutsu_kaisen/entity/SimpleDomainEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/SimpleDomainEntity.java
@@ -122,7 +122,7 @@ public class SimpleDomainEntity extends Entity {
         ISorcererData cap = this.getOwner().getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
         cap.delayTickEvent(() -> {
             invuln = false;
-        }, 7);
+        }, 5);
         return true;
     }
 
