d7136d50ccf4baf931ba2b2a6d2c8235886729e3
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-19 19:49:14 -0500

    Update Wings.java

--- src/main/java/radon/jujutsu_kaisen/ability/idle_transfiguration/Wings.java
+++ src/main/java/radon/jujutsu_kaisen/ability/idle_transfiguration/Wings.java
@@ -10,7 +10,7 @@ import radon.jujutsu_kaisen.item.JJKItems;
 import radon.jujutsu_kaisen.util.RotationUtil;
 
 public class Wings extends Transformation {
-    private static final float SPEED = 0.02F;
+    private static final float SPEED = 0.01F;
 
     @Override
     public boolean isScalable(LivingEntity owner) {
@@ -46,7 +46,7 @@ public class Wings extends Transformation {
 
     @Override
     public float getCost(LivingEntity owner) {
-        return 2.0F;
+        return 1.0F;
     }
 
     @Override
