d4e56d0566824fa25fb17203959e10fd8a6671e2
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-24 01:00:13 -0500

    Update TranquilDeerEntity.java

--- src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/TranquilDeerEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/TranquilDeerEntity.java
@@ -104,7 +104,7 @@ public class TranquilDeerEntity extends TenShadowsSummon {
 
     public static AttributeSupplier.Builder createAttributes() {
         return SorcererEntity.createAttributes()
-                .add(Attributes.MAX_HEALTH, 3 * 15.0D)
+                .add(Attributes.MAX_HEALTH, 2 * 12.0D)
                 .add(Attributes.ARMOR, 6.0D)
                 .add(Attributes.ATTACK_DAMAGE, 5 * 2.0D);
     }
