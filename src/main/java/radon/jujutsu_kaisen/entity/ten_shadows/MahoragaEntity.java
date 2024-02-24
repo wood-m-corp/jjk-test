0b0bb928143dc6c827327e0d217af62983f5c3b9
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-24 14:31:46 -0500

    Update MahoragaEntity.java

--- src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/MahoragaEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/ten_shadows/MahoragaEntity.java
@@ -130,7 +130,7 @@ public class MahoragaEntity extends TenShadowsSummon {
 
     public static AttributeSupplier.Builder createAttributes() {
         return SorcererEntity.createAttributes()
-                .add(Attributes.MAX_HEALTH, 6 * 14.0D)
+                .add(Attributes.MAX_HEALTH, 5 * 15.0D)
                 .add(Attributes.ARMOR, 20.0D)
                 .add(Attributes.ATTACK_DAMAGE, 6 * 2.5D);
     }
