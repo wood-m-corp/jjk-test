73379559c382dc911c1fa3c661b525b68e248181
codeblad <152137808+codeblad@users.noreply.github.com>
2024-02-28 01:28:29 -0500

    Update MaximumUzumakiProjectile.java

--- src/main/java/radon/jujutsu_kaisen/entity/projectile/MaximumUzumakiProjectile.java
+++ src/main/java/radon/jujutsu_kaisen/entity/projectile/MaximumUzumakiProjectile.java
@@ -29,8 +29,8 @@ import software.bernie.geckolib.core.animation.AnimatableManager;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class MaximumUzumakiProjectile extends JujutsuProjectile implements GeoEntity {
-    private static final int DELAY = 20;
-    private static final double RANGE = 30.0D;
+    private static final int DELAY = 25;
+    private static final double RANGE = 25.0D;
     private static final float MAX_POWER = 20.0F;
 
     private float power;
