7bde3b8bc81ce5c1a2a5808eb698615ae61d88fe
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-27 21:34:58 -0500

    Update BlastAway.java

--- src/main/java/radon/jujutsu_kaisen/ability/cursed_speech/BlastAway.java
+++ src/main/java/radon/jujutsu_kaisen/ability/cursed_speech/BlastAway.java
@@ -26,7 +26,7 @@ import java.util.List;
 public class BlastAway extends Ability {
     private static final double RANGE = 25.0D;
     private static final double RADIUS = 2.0D;
-    private static final float DAMAGE = 14.0F;
+    private static final float DAMAGE = 11.0F;
     private static final double LAUNCH_POWER = 7.5D;
 
     @Override
