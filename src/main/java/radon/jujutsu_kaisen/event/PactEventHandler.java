5d40fff5304c821aa211afbb23e738c63abf4438
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-02-27 20:55:10 -0500

    Update PactEventHandler.java

--- src/main/java/radon/jujutsu_kaisen/event/PactEventHandler.java
+++ src/main/java/radon/jujutsu_kaisen/event/PactEventHandler.java
@@ -33,8 +33,8 @@ public class PactEventHandler {
                     ISorcererData cap = attacker.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
 
                     if (cap.hasBindingVow(BindingVow.RECOIL)) {
-                        attacker.hurt(JJKDamageSources.self(victim), event.getAmount() * 0.25F);
-                        event.setAmount(event.getAmount() * 1.25F);
+                        attacker.hurt(JJKDamageSources.self(victim), event.getAmount() * 0.2F);
+                        event.setAmount(event.getAmount() * 1.15F);
                     }
                 }
             }
