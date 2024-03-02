b5aede9892cfbb7c42c52c14a9cf775415628a1a
BRUHSIF38 <151086900+BRUHSIF38@users.noreply.github.com>
2024-03-02 12:03:15 -0500

    Update TransfiguredSoulSmallEntity.java

--- src/main/java/radon/jujutsu_kaisen/entity/idle_transfiguration/TransfiguredSoulSmallEntity.java
+++ src/main/java/radon/jujutsu_kaisen/entity/idle_transfiguration/TransfiguredSoulSmallEntity.java
@@ -31,8 +31,8 @@ public class TransfiguredSoulSmallEntity extends TransfiguredSoulVariantEntity {
     public static AttributeSupplier.Builder createAttributes() {
         return SorcererEntity.createAttributes()
                 .add(Attributes.MAX_HEALTH, 20.0F)
-                .add(Attributes.ARMOR, 4.0D)
-                .add(Attributes.ATTACK_DAMAGE, 4 * 1.5D);
+                .add(Attributes.ARMOR, 8.0D)
+                .add(Attributes.ATTACK_DAMAGE, 5 * 1.5D);
     }
 
     @Override
